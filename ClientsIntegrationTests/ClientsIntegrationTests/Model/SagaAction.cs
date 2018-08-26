e5b068d983d961cf90956a825b3e59d8ab1cd9dd
aleksmorozova <aleks_morozova91@mail.ru>
2018-08-26 19:33:55 +0300

    add project
    add project

---
+++ ClientsIntegrationTests/ClientsIntegrationTests/Model/SagaAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientsIntegrationTests.model
+{
+    class SagaAction
+    {
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ ClientsIntegrationTests/ClientsIntegrationTests/Model/Test.cs
@@ -0,0 +1,16 @@
+﻿using ClientsIntegrationTests.SetUp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientsIntegrationTests.model
+{
+    class Test
+    {
+        public string Name { get; set; }
+        public TestType Type { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
